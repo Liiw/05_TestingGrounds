8042c679fcedd7e9da54643234814c1d4f959b53
Liiw <sander.liivandi@gmail.com>
2019-04-01 21:21:23 +0300

    Patrol routes working in C++

--- Source/S05_TestingGrounds/Private/ChooseNextWaypoint.cpp
+++ Source/S05_TestingGrounds/Private/ChooseNextWaypoint.cpp
@@ -2,13 +2,30 @@
 
 #include "Public/ChooseNextWaypoint.h"
 #include "BehaviorTree/BlackboardComponent.h"
+#include "AIController.h"
+#include "Public/PatrollingGuard.h"
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent & OwnerComp, uint8 * NodeMemory)
 {
+	//TODO protect againts empty patrol routes
+
+	/// Get the patrol points
+	auto AIController = OwnerComp.GetAIOwner();
+	auto ControlledPawn = AIController->GetPawn();
+	auto PatrollingGuard = Cast<APatrollingGuard>(ControlledPawn);
+	auto PatrolPoints = PatrollingGuard->PatrolPointsCPP;
+
+	///Set next waypoint
 	auto BlacboardComp = OwnerComp.GetBlackboardComponent();
 	auto Index = BlacboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
+	BlacboardComp->SetValueAsObject(WaypointKey.SelectedKeyName, PatrolPoints[Index]);
 
-	UE_LOG(LogTemp, Warning, TEXT("Index: %i"), Index);
+	///Cycle the index
+	auto NextIndex = (Index + 1) % PatrolPoints.Num();
+	BlacboardComp->SetValueAsInt(IndexKey.SelectedKeyName, NextIndex);
+
+
+	///UE_LOG(LogTemp, Warning, TEXT("Index: %i"), Index);
 
 	return EBTNodeResult::Succeeded;
 }
